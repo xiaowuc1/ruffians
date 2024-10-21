d875d0511312201de4597021501e65c86857bcad
Geoffry Song <goffrie@gmail.com>
2024-10-21 03:08:07 +0000

    nix: update pins

--- build.nix
+++ build.nix
@@ -3,13 +3,13 @@ let
     owner = "NixOS";
     repo = "nixpkgs";
     rev = "e0da498ad77ac8909a980f07eff060862417ccf7";
-    sha256 = "sha256-evZzmLW7qoHXf76VCepvun1esZDxHfVRFUJtumD7L2M=";
+    hash = "sha256-evZzmLW7qoHXf76VCepvun1esZDxHfVRFUJtumD7L2M=";
   };
   strawberry = (import <nixpkgs> {}).fetchFromGitHub {
     owner = "goffrie";
     repo = "strawberry";
-    rev = "209d11f16ece71c58fc021a333577b7eeafe0874";
-    sha256 = "sha256-S1gB2HGChOK1+fk/aReDKcPgSxwr/1fc8D6duGIOrMw=";
+    rev = "0da2ebc430482962da4e9de5586ce9806770f390";
+    hash = "sha256-6Ntsbe97vpMbO7SprFns2H2pR/DkVpd3E3ArOBIlOYI=";
   };
 in { pkgs ? import pinnedNixpkgs {} }:
 let
